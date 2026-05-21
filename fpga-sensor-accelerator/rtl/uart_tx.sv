2d119473dfb3a9b3afecaa25643f52f508e053bc
Harish Vijayakakumar <harish.mbes@gmail.com>
2026-05-21 09:33:40 -0400

    Updated uart_tx so that the shift only happens on the correct baud tick. I chose baud 115200 for the DE-10's 50Mhz clock, so the baud_div became ~434. Now the shift only happens every 434 clocks, until the whole input is done. I also updated the testbench to last longer so that you could see all 10 bits being shifted.

--- fpga-sensor-accelerator/rtl/uart_tx.sv
+++ fpga-sensor-accelerator/rtl/uart_tx.sv
@@ -7,13 +7,22 @@ input logic clk,
 input logic send,
 
 output logic tx //the output is usually in the form of 
-									 // stop, 8 bits data, start from left to right
+					 // stop, 8 bits data, start from left to right
 
 );
 
 logic [9:0] shift_reg = 10'b1111111111;
 logic [3:0] bit_counter = 10;
 
+// baud div is 50,000,000 / 115200 baud = ~434
+localparam BAUD_DIV = 434; //localparam is like a static final variable in a class
+logic [9:0] baud_counter = 0;
+logic baud_tick = 0;
+
+//this module sets the output to each bit of the frame, and returns it when send is not 1 until the full uart input is done
+//uart stands for universal asynchronous reciever transmitter
+// it translates data from the fpga to the computer, so if the fpga wants to send 123, uart sends it as bits 00110001, 00110010, 00110011
+// uart sends ascii values, that why the binary bits have extra 1s, its its ascii translation as a CHARACTER
 
 always_ff @(posedge clk) begin
 
@@ -21,23 +30,46 @@ always_ff @(posedge clk) begin
 
 	shift_reg <= {1'b1, hex_input, 1'b0}; //filling out the full uart response
 	bit_counter<= 0; //resetting the bit counter
-	tx <= 1'b0; //output the start bit
+	tx <= 1'b0; //output the start bit  which is 0
 	end
 
-	else if(bit_counter < 10) begin //if send is not 1
+	else if(bit_counter < 10 && baud_tick) begin //if send is not 1 AND baud_tick is 1(we are at the correct baud)
+	
 	tx <= shift_reg[0]; //setting the output tx to the shift register
+	
 	shift_reg <= shift_reg >> 1; //shifting the register right by 1, (diving by 2 in decimal)
+	
 	bit_counter <= bit_counter + 1; //adding 1 to the bit counter
 	end
 	
 	else begin //if send is not 1 and bit counter is 10 or greater (meaning we have finished going through the values)
 	tx <= 1'b1; //output is just 1
 
- //this module sets the output to each bit of the frame, and returns it when send is not 1 until the full uart input is done
-//uart stands for universal asynchronous reciever transmitter
-// it translates data from the fpga to the computer, so if the fpga wants to send 123, uart sends it as bits 00110001, 00110010, 00110011
-// uart sends ascii values, that why the binary bits have extra 1s, its its ascii translation as a CHARACTER
+ 
 	end
 	
 end
+
+
+//baud counter
+//updates the baud_tick as if it were a clock, only when the baud_div is hit on the positive edge of a clk
+
+
+always_ff @(posedge clk) begin 
+
+if (baud_counter == BAUD_DIV-1) begin //if baud_counter is about to hit the BAUD_DIV
+
+	baud_counter<= 0; //reset counter
+	baud_tick <= 1; //set baud_tick to 1
+	
+end
+	else begin //if not at baud_DIV
+	
+		baud_counter <= baud_counter + 1; //Keep incrementing baud_counter
+		baud_tick <= 0; //tick is still 0
+		
+	end
+
+end
+
 endmodule
