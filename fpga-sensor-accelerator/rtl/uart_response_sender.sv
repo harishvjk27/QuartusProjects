253791675028fe13e575368309082b085a88196f
Harish Vijayakakumar <harish.mbes@gmail.com>
2026-06-09 09:34:47 -0400

    Refactored UART Response sender to be more generic and have a scalable command engine

--- fpga-sensor-accelerator/rtl/uart_response_sender.sv
+++ fpga-sensor-accelerator/rtl/uart_response_sender.sv
@@ -24,351 +24,219 @@ CMD_HELP,
 CMD_DEBUG,
 CMD_FILTER,
 CMD_LED
-} cmd_type;
+} command_t;
 
-//creating new object to represent letters in the states for HELP, DEBUG, FILTER, LED
-typedef enum logic [19:0]  {
+//current state 
+command_t current_command;
 
-IDLE,
+//creating new states to represent what is currently going on 
+typedef enum logic [1:0]  {
 
-	 // HELP
-    HELP_SEND_H,
-    HELP_WAIT_H,
-
-    HELP_SEND_E,
-    HELP_WAIT_E,
-
-    HELP_SEND_L,
-    HELP_WAIT_L,
-
-    HELP_SEND_P,
-    HELP_WAIT_P,
-
-    // DEBUG
-    DEBUG_SEND_D,
-    DEBUG_WAIT_D,
-
-    DEBUG_SEND_E,
-    DEBUG_WAIT_E,
-
-    DEBUG_SEND_B,
-    DEBUG_WAIT_B,
-
-    DEBUG_SEND_U,
-    DEBUG_WAIT_U,
-
-    DEBUG_SEND_G,
-    DEBUG_WAIT_G,
-
-	// FILTER
-    FILTER_SEND_F,
-    FILTER_WAIT_F,
-
-    FILTER_SEND_I,
-    FILTER_WAIT_I,
-
-    FILTER_SEND_L,
-    FILTER_WAIT_L,
-
-    FILTER_SEND_T,
-    FILTER_WAIT_T,
-	 
-	 FILTER_SEND_E,
-    FILTER_WAIT_E,
-	 
-	 FILTER_SEND_R,
-    FILTER_WAIT_R,
-
-    // LED
-    LED_SEND_L,
-    LED_WAIT_L,
-
-    LED_SEND_E,
-    LED_WAIT_E,
-
-    LED_SEND_D,
-    LED_WAIT_D
-
+	 IDLE,
+	 SEND_CHAR,
+	 WAIT_DONE,
+	 NEXT_CHAR
 
 } state_t;
 
-//two state_t objects to hold current and next state
-
-state_t current_state = IDLE;
+//state enums
 state_t next_state;
+state_t current_state = IDLE;
 
-
-//HELP CASE CODE
-
-// case logic for the next-state with current state defaulting at IDLE, for each command
-always_comb begin
-
-    next_state = current_state;
-
-    case(current_state)
-
-        IDLE: begin
-            if(help_cmd)
-                next_state = HELP_SEND_H;
-            else if(debug_cmd)
-                next_state = DEBUG_SEND_D;
-				else if(filter_cmd)
-					 next_state = FILTER_SEND_F;
-				else if(led_cmd)
-					 next_state = LED_SEND_L;
-        end
-
-        // HELP
-
-        HELP_SEND_H:
-            next_state = HELP_WAIT_H;
-
-        HELP_WAIT_H:
-            if(tx_done)
-                next_state = HELP_SEND_E;
-
-        HELP_SEND_E:
-            next_state = HELP_WAIT_E;
-
-        HELP_WAIT_E:
-            if(tx_done)
-                next_state = HELP_SEND_L;
-
-        HELP_SEND_L:
-            next_state = HELP_WAIT_L;
-
-        HELP_WAIT_L:
-            if(tx_done)
-                next_state = HELP_SEND_P;
-
-        HELP_SEND_P:
-            next_state = HELP_WAIT_P;
-
-        HELP_WAIT_P:
-            if(tx_done)
-                next_state = IDLE;
-
-        // DEBUG
-
-        DEBUG_SEND_D:
-            next_state = DEBUG_WAIT_D;
-
-        DEBUG_WAIT_D:
-            if(tx_done)
-                next_state = DEBUG_SEND_E;
-
-        DEBUG_SEND_E:
-            next_state = DEBUG_WAIT_E;
-
-        DEBUG_WAIT_E:
-            if(tx_done)
-                next_state = DEBUG_SEND_B;
-
-        DEBUG_SEND_B:
-            next_state = DEBUG_WAIT_B;
-
-        DEBUG_WAIT_B:
-            if(tx_done)
-                next_state = DEBUG_SEND_U;
-
-        DEBUG_SEND_U:
-            next_state = DEBUG_WAIT_U;
-
-        DEBUG_WAIT_U:
-            if(tx_done)
-                next_state = DEBUG_SEND_G;
-
-        DEBUG_SEND_G:
-            next_state = DEBUG_WAIT_G;
-
-        DEBUG_WAIT_G:
-            if(tx_done)
-                next_state = IDLE;
-					 
-		  //FILTER
-			FILTER_SEND_F:
-            next_state = FILTER_WAIT_F;
-				
-			FILTER_WAIT_F:
-				if(tx_done)
-            next_state = FILTER_SEND_I;
-				
-			FILTER_SEND_I:
-            next_state = FILTER_WAIT_I;
-				
-			FILTER_WAIT_I:
-				if(tx_done)
-            next_state = FILTER_SEND_L;
-				
-			FILTER_SEND_L:
-            next_state = FILTER_WAIT_L;
-				
-			FILTER_WAIT_L:
-				if(tx_done)
-            next_state = FILTER_SEND_T;
-				
-			FILTER_SEND_T:
-            next_state = FILTER_WAIT_T;
-				
-			FILTER_WAIT_T:
-				if(tx_done)
-            next_state = FILTER_SEND_E;
-				
-			FILTER_SEND_E:
-            next_state = FILTER_WAIT_E;
-				
-			FILTER_WAIT_E:
-				if(tx_done)
-            next_state = FILTER_SEND_R;
-				
-			FILTER_SEND_R:
-            next_state = FILTER_WAIT_R;
-				
-			FILTER_WAIT_R:
-				if(tx_done)
-            next_state = IDLE;
-			
-			//LED
-			LED_SEND_L:
-            next_state = LED_WAIT_L;
-				
-			LED_WAIT_L:
-				if(tx_done)
-            next_state = LED_SEND_E;
-				
-			LED_SEND_E:
-            next_state = LED_WAIT_E;
-				
-			LED_WAIT_E:
-				if(tx_done)
-            next_state = LED_SEND_D;
-				
-			LED_SEND_D:
-            next_state = LED_WAIT_D;
-				
-			LED_WAIT_D:
-				if(tx_done)
-            next_state = IDLE;
-			
-        
-        default:
-            next_state = IDLE;
-
-    endcase
-
-end
-
+//internal wires to hold values
+logic[3:0] char_index;
+logic[3:0] message_length;
+logic[7:0] current_char;
 
 //Outputting the letters depending on the state
+always_comb begin 
+
+	current_char = 8'h00;
+	
+	case (current_command) 
+	
+	
+	CMD_HELP:
+	
+	
+			case(char_index)
+			0: current_char = "H";
+			1: current_char = "E";
+			2: current_char = "L";
+			3: current_char = "P";
+			endcase
+	
+	CMD_DEBUG:
+	
+	
+		case(char_index)
+			0: current_char = "D";
+			1: current_char = "E";
+			2: current_char = "B";
+			3: current_char = "U";
+			4: current_char = "G";
+			endcase
+			
+	CMD_FILTER:
+	
+	
+		case(char_index)
+			0: current_char = "F";
+			1: current_char = "I";
+			2: current_char = "L";
+			3: current_char = "T";
+			4: current_char = "E";
+			5: current_char = "R";
+			endcase
+			
+	CMD_LED:
+
+	
+		case(char_index)
+			0: current_char = "L";
+			1: current_char = "E";
+			2: current_char = "D";
+		endcase
+		
+	endcase
+
+end
+
+
+//logic for message length
+always_comb begin
+
+message_length = 4'h0;
+
+	case (current_command)
+		
+		CMD_HELP:
+		
+		message_length = 4;
+		
+		CMD_DEBUG:
+		
+		message_length = 5;
+		
+		CMD_FILTER:
+		
+		message_length = 6;
+		
+		CMD_LED:
+		
+		message_length = 3;
+			
+	endcase
+end
+
+//updating values sequentially so correct char is set at the correct time
+always_ff @(posedge clk) begin
+
+
+current_state <= next_state;
+
+if(current_state == IDLE) begin
+
+	if(help_cmd) begin
+	current_command <= CMD_HELP;
+	char_index<=0;
+	
+	end
+	
+	if(debug_cmd) begin
+	current_command <= CMD_DEBUG;
+	char_index<=0;
+	
+	end
+	
+	if(filter_cmd) begin
+	current_command <= CMD_FILTER;
+	char_index<=0;
+	
+	end
+	
+	if(led_cmd) begin
+	current_command <= CMD_LED;
+	char_index<=0;
+	
+	end
+end
+
+else if(current_state == NEXT_CHAR && char_index < message_length-1)
+
+char_index <= char_index + 1;
+
+
+end
+
+//state transitions
+always_comb begin 
+
+next_state = current_state;
+
+case(current_state)
+
+	IDLE: begin
+	
+		if(help_cmd)
+			next_state = SEND_CHAR;
+			
+		else if(debug_cmd) 
+			next_state = SEND_CHAR;
+			
+		else if(filter_cmd)
+			next_state = SEND_CHAR;
+			
+		else if(led_cmd)
+			next_state = SEND_CHAR;
+			
+		
+	end
+	
+	SEND_CHAR:
+		next_state = WAIT_DONE;
+		
+	WAIT_DONE: begin
+	
+		if(tx_done)
+		
+		next_state = NEXT_CHAR;
+		
+		end
+		
+	NEXT_CHAR: begin
+		
+		if (char_index == message_length -1)
+		
+			next_state = IDLE;
+			
+		else 
+			next_state = SEND_CHAR;
+			
+	end
+	
+endcase
+
+end
+
+//setting the data to the current output
 
 always_comb begin
 
-    response_tx_start = 1'b0;
-    response_tx_data  = 8'h00;
+response_tx_start = 0;
+response_tx_data = 0;
 
-    case(current_state)
+case(current_state)
 
-        HELP_SEND_H: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "H";
-        end
+	SEND_CHAR: begin
+	
+	response_tx_start = 1;
+	response_tx_data = current_char;
+	end
 
-        HELP_SEND_E: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "E";
-        end
-
-        HELP_SEND_L: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "L";
-        end
-
-        HELP_SEND_P: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "P";
-        end
-
-        DEBUG_SEND_D: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "D";
-        end
-
-        DEBUG_SEND_E: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "E";
-        end
-
-        DEBUG_SEND_B: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "B";
-        end
-
-        DEBUG_SEND_U: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "U";
-        end
-
-        DEBUG_SEND_G: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "G";
-        end
-		  
-		  //FILTER
-		  
-		  FILTER_SEND_F: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "F";
-        end
-		  
-		  FILTER_SEND_I: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "I";
-        end
-		  
-		  FILTER_SEND_L: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "L";
-        end
-		  
-		  FILTER_SEND_T: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "T";
-        end
-		  
-		  FILTER_SEND_E: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "E";
-        end
-		  
-		  FILTER_SEND_R: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "R";
-        end
-		  
-		  LED_SEND_L: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "L";
-        end
-		  
-		  LED_SEND_E: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "E";
-        end
-		  
-		  LED_SEND_D: begin
-            response_tx_start = 1'b1;
-            response_tx_data  = "D";
-        end
-
-    endcase
-
+	endcase
 end
-
-//when clock ticks positive, current state equals next state
-always_ff @(posedge clk) begin
-current_state <= next_state;
-
-end
-
 
 endmodule
+
+
