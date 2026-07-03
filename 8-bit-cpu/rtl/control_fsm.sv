0be3bf05dbebbcdc1cb54905ae3407849b6efdf7
Harish Vijayakakumar <harish.mbes@gmail.com>
2026-07-03 12:27:51 -0400

    Finished FSM logic to go in correct sequence and update enable / writes.

--- 8-bit-cpu/rtl/control_fsm.sv
+++ 8-bit-cpu/rtl/control_fsm.sv
@@ -21,5 +21,58 @@ WRITEBACK
 
 } state_t;
 
+state_t current_state = RESET;
+state_t next_state;
 
+always_ff @(posedge clk) begin //updates current state every clock cycle
+
+if (reset)
+
+	current_state <= RESET;
+	
+else
+	current_state <= next_state;
+end
+
+always_comb begin
+
+
+	next_state = current_state;
+	
+	case(current_state) 
+	
+		RESET : 
+			next_state = FETCH;
+			
+		FETCH : 
+			next_state = DECODE;
+			
+		DECODE : 
+			next_state = EXECUTE;
+			
+		EXECUTE:
+			next_state = WRITEBACK;
+			
+		WRITEBACK : 
+			next_state = FETCH;
+			
+		endcase
+		
+	end
+
+	always_comb begin
+	
+	pc_enable = 1'b0;
+	reg_write_en = 1'b0;
+	
+	case (current_state)
+	
+		FETCH :
+			pc_enable = 1'b1;
+		WRITEBACK:
+			reg_write_en = 1'b1;
+		endcase
+	end
+	
+	
 endmodule
